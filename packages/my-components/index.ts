04f58da6c50a316be6509e7e5cded670521b29da
noah.choi <noah.choi@goorm.io>
2024-07-21 23:33:33 +0900

    feat: NoBody 컴포넌트 추가

--- packages/my-components/index.ts
+++ packages/my-components/index.ts
@@ -1,3 +1,4 @@
 export { default as Dialog } from './src/Dialog';
 export { default as Button } from './src/Button';
-// export { default as Calendar } from './src/Calendar';
+export { default as Calendar } from './src/Calendar';
+export { default as NoBody } from './src/NoBody';
