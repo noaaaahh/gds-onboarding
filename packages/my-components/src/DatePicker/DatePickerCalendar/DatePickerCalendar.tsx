b248e27e2cc62ab78efc72810d5d71c3f7156d9e
noah.choi <noah.choi@goorm.io>
2024-08-02 12:35:34 +0900

    fix(DatePickerCalendar): onChange 관련 버그 수정

--- packages/my-components/src/DatePicker/DatePickerCalendar/DatePickerCalendar.tsx
+++ packages/my-components/src/DatePicker/DatePickerCalendar/DatePickerCalendar.tsx
@@ -1,33 +1,36 @@
 import React, { ComponentProps, useEffect } from 'react';
-import { LooseValue } from 'react-calendar/dist/cjs/shared/types';
 
 import Calendar from '../../Calendar';
 import { useDatePicker } from '../DatePicker.context';
+import { getCalendarValue, getNextDate } from './DatePickerCalendar.utils';
 import styles from './DatePickerCalendar.module.scss';
-import { ModeDate } from './DatePickerCalendar.types';
 
 const DatePickerCalendar = ({
     maxDate,
     minDate,
     ...props
 }: ComponentProps<typeof Calendar>) => {
-    const { date, handleChange, mode, locale, initializeRange } =
-        useDatePicker();
+    const { date, handleChange, locale, initializeRange } = useDatePicker();
+
+    const handleClickDay = (selectedDate: Date) => {
+        const nextDate = getNextDate(date, selectedDate);
+
+        handleChange(nextDate);
+    };
 
     useEffect(() => {
-        initializeRange({ maxDate, minDate });
+        initializeRange({ maxDate: maxDate || null, minDate: minDate || null });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <Calendar
-            value={date as LooseValue} // 라이브러리 타입을 변형해서 쓰는 게 위험할 수 있다.
+            value={getCalendarValue(date)}
             allowPartialRange
-            onChange={(date) => handleChange(date as ModeDate<typeof mode>)}
+            onClickDay={handleClickDay}
             locale={locale}
             minDate={minDate}
             maxDate={maxDate}
-            selectRange={mode === 'range'}
             className={styles.calendar}
             {...props}
         />
---
+++ packages/my-components/src/DatePicker/DatePickerCalendar/DatePickerCalendar.utils.ts
@@ -0,0 +1,24 @@
+import { LooseValue } from 'react-calendar/dist/cjs/shared/types';
+import { DateType } from '../DatePicker.types';
+import { getSortedDates, isDateValue } from '../DatePicker.utils';
+
+export const getNextDate = (date: DateType, currentDate: Date) => {
+    if (isDateValue(date)) return currentDate;
+
+    const { from, to } = date;
+
+    if (from && !to) return getSortedDates({ from, to: currentDate });
+    else if (!from && to) return getSortedDates({ from: currentDate, to });
+    else return getSortedDates({ from: currentDate, to: null });
+};
+
+export const getCalendarValue = (date: DateType): LooseValue => {
+    if (isDateValue(date)) return date;
+
+    const { from, to } = date;
+
+    if (from && !to) return from;
+    if (!from && to) return to;
+
+    return [date.from, date.to];
+};
